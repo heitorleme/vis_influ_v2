4badc9de359b96c0befa4e8b98ded39167bf1ce9
heitorleme <78544347+heitorleme@users.noreply.github.com>
2025-07-22 20:01:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -41,9 +41,9 @@ with abas[0]:
         st.session_state.df_cidades = pd.DataFrame()
 
 		if "influencers_dados" not in st.session_state:
-		    st.session_state.influencers_dados = {}
-		    st.session_state.influencers_nomes = []
-		    st.session_state.df_cidades = pd.DataFrame()
+                    st.session_state.influencers_dados = {}
+                    st.session_state.influencers_nomes = []
+                    st.session_state.df_cidades = pd.DataFrame()
 
         for file in uploaded_files:
             filename = file.name
