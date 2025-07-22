fbe38a17c64b6060cff78dfab20dec287acb5175
heitorleme <78544347+heitorleme@users.noreply.github.com>
2025-07-22 20:19:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -213,8 +213,8 @@ with abas[4]:
 ############ Resumo dos Influenciadores ###############
 with abas[1]:
     if "influencers_dados" not in st.session_state:
-    st.warning("⚠️ Nenhum arquivo foi carregado. Volte para a aba 1 e envie os arquivos JSON.")
-    st.stop()  # evita execução do restante
+        st.warning("⚠️ Nenhum arquivo foi carregado. Volte para a aba 1 e envie os arquivos JSON.")
+        st.stop()  # evita execução do restante
     
     df_resumo = consolidar_resumo_influenciadores(
     dados_influencers=st.session_state.influencers_dados,
